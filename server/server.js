b216b9bc5670b6f66f87bbd324057cce55a119fb
Pratyukt <pratyuktnag@gmail.com>
2024-10-23 15:38:37 +0530

    23 october partials , views , hbs and etc

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express= require("express");
+const connectDb= require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors= require ("cors");
+const path = require("path");
+const hbs = require("hbs");
+
+// env file config
+const dotenv = require("dotenv");
+dotenv.config();
+
+connectDb();
+const app = express();
+const port= process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+//Error handling
+app.use(errorHandler);
+
+//Routes below
+app.get("/",(req,res)=>{
+    res.send("working")
+});
+
+app.set('view engine' , 'hbs');
+
+app.get("/home",(req,res)=>{
+    res.render("home",{
+        users: [
+            { username: "aaa", date: "23-10-2024", subject: "Maths" },
+            { username: "bbb", date: "23-10-2014", subject: "Science" },
+            { username: "ccc", date: "23-10-2004", subject: "Hindi" }
+        ]
+    })
+})
+
+hbs.registerPartials(path.join(__dirname, '/views/partials/'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
